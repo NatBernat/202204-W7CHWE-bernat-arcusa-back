b715d575c816fc6910b5ee0dda812474f1dcf7d9
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-20 22:05:41 +0200

    Added db connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 require("dotenv").config();
-
+const debug = require("debug")("social-api:root");
+const chalk = require("chalk");
+const connectDB = require("../../202204-W7CH1-bernat-arcusa-boris-randebrock/db");
 const startServer = require("./server/startServer");
 
 const port = process.env.API_PORT || 5000;
+const mongoConnection = process.env.MONGODB_STRING;
 
-startServer(port);
+(async () => {
+  try {
+    await connectDB(mongoConnection);
+    await startServer(port);
+  } catch (error) {
+    debug(chalk.red("Error: ", error.message));
+  }
+})();
